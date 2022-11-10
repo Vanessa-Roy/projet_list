5721de323fbddba01a763c11946423d29a017547
vanessa <vr.vanessa.roy@gmail.com>
2022-11-10 17:24:03 +0100

    F5 done (button reset)

--- projet_list.js
+++ projet_list.js
@@ -1,10 +1,13 @@
-const listArticlesAdded = [];
-const listArticlesChecked = [];
+let listArticlesAdded = [];
+let listArticlesChecked = [];
 const button = document.getElementById("button");
 button.addEventListener("click",function(event){
     event.preventDefault();
 });
+const removeAllAddedArticlesButton = document.getElementById('buttonForSupprAddedArticles');
+const removeAllCheckedArticlesButton = document.getElementById('buttonForSupprCheckedArticles');
 function addToTheList() {
+    removeAllAddedArticlesButton.removeAttribute("hidden");
     const articleAdded = document.getElementById("articleAdded");
     const article = document.getElementById("articleToAdd").value;
     listArticlesAdded.push(article);
@@ -22,21 +25,24 @@ function addToTheList() {
 }
 function addToTheCheckedList(e) {
     if(e.checked === true) {
+        removeAllCheckedArticlesButton.removeAttribute("hidden");
         listArticlesChecked.push(e.parentElement.textContent);
         e.removeAttribute("onclick","addToTheCheckedList(this)");
         e.setAttribute("type","button");
-        e.setAttribute("value","remettre dans les articles ajoutés");
+        e.setAttribute("value","✅");
         e.setAttribute("onclick","removeAndAddToTheList(this)")
         let trashButton = document.createElement('input');
         trashButton.setAttribute("type","button");
-        trashButton.setAttribute("value","supprimer l'article sélectionné");
+        trashButton.setAttribute("value","🗑️");
         trashButton.setAttribute("onclick","removeTheArticleChecked(this)")
-        let li = document.createElement('li');
+        let li = e.parentElement.parentElement;
         e.parentElement.appendChild(trashButton);
         li.appendChild(e.parentElement);
         articleChecked.appendChild(li);
         let indexOfArticleAdded = listArticlesAdded.indexOf(e.parentElement.textContent);
         let articleAddedRemoved = listArticlesAdded.splice(indexOfArticleAdded, 1);
+        ShowTheRemoveButton();
+
         console.log("articles ajoutés : ", listArticlesAdded);
         console.log("articles sélectionnés : ", listArticlesChecked);
     } else {
@@ -45,7 +51,7 @@ function addToTheCheckedList(e) {
 }
 function removeAndAddToTheList(e) {
     listArticlesAdded.push(e.parentElement.textContent);
-    let li = document.createElement('li');
+    let li = e.parentElement.parentElement;
     let checkbox = document.createElement('input');
     checkbox.setAttribute("type","checkbox");
     checkbox.setAttribute("onclick","addToTheCheckedList(this)");
@@ -57,15 +63,50 @@ function removeAndAddToTheList(e) {
     let indexOfArticleChecked = listArticlesChecked.indexOf(e.parentElement.textContent);
     let articleCheckedRemoved = listArticlesChecked.splice(indexOfArticleChecked, 1);
     e.parentElement.innerHTML = "";
+    ShowTheRemoveButton();
     console.log("articles ajoutés : ", listArticlesAdded);
     console.log("articles sélectionnés : ", listArticlesChecked);
 }
 function removeTheArticleChecked(e) {
-    //supprimer l'article du tableau article sélectionnés
     let indexOfArticleChecked = listArticlesChecked.indexOf(e.parentElement.textContent);
     let articleCheckedRemoved = listArticlesChecked.splice(indexOfArticleChecked, 1);
-    //supprimer l'article de la zone d'affichage
-    e.parentElement.innerHTML = "";
+    e.parentElement.parentElement.remove();
+    ShowTheRemoveButton();
     console.log("articles ajoutés : ", listArticlesAdded);
     console.log("articles sélectionnés : ", listArticlesChecked);
 }
+function removeAllArticlesChecked() {
+    let ul = document.getElementById("articleChecked")
+    ul.innerHTML = "";
+    listArticlesChecked = [];
+    removeAllCheckedArticlesButton.setAttribute("hidden","true");
+    console.log("articles ajoutés : ", listArticlesAdded);
+    console.log("articles sélectionnés : ", listArticlesChecked);
+}
+function removeAllArticlesAdded() {
+    let ul = document.getElementById("articleAdded")
+    ul.innerHTML = "";
+    listArticlesAdded = [];
+    removeAllAddedArticlesButton.setAttribute("hidden","true");
+    console.log("articles ajoutés : ", listArticlesAdded);
+    console.log("articles sélectionnés : ", listArticlesChecked);
+}
+function ShowTheRemoveButton() {
+    if (listArticlesAdded.length != 0 && listArticlesChecked.length != 0) {
+        removeAllAddedArticlesButton.removeAttribute("hidden");
+        removeAllCheckedArticlesButton.removeAttribute("hidden");
+        console.log("dans le listArticlesAdded.length != 0 && listArticlesChecked.length != 0","taille fichier article ajouté : ", listArticlesAdded.length,"taille fichier article checké : ",listArticlesChecked.length);
+    } else if (listArticlesAdded.length == 0 && listArticlesChecked.length != 0) {
+        removeAllAddedArticlesButton.setAttribute("hidden","true");
+        removeAllCheckedArticlesButton.removeAttribute("hidden");
+        console.log("dans le listArticlesAdded.length == 0 && listArticlesChecked.length != 0","taille fichier article ajouté : ", listArticlesAdded.length,"taille fichier article checké : ",listArticlesChecked.length);
+    } else if (listArticlesAdded.length != 0 && listArticlesChecked.length == 0) {
+            removeAllAddedArticlesButton.removeAttribute("hidden");
+            removeAllCheckedArticlesButton.setAttribute("hidden","true");
+            console.log("dans le listArticlesAdded.length != 0 && listArticlesChecked.length == 0","taille fichier article ajouté : ", listArticlesAdded.length,"taille fichier article checké : ",listArticlesChecked.length);
+    } else if (listArticlesAdded.length == 0 && listArticlesChecked.length == 0) {
+            removeAllAddedArticlesButton.setAttribute("hidden","true");
+            removeAllCheckedArticlesButton.setAttribute("hidden","true");
+            console.log("dans le listArticlesAdded.length == 0 && listArticlesChecked.length == 0","taille fichier article ajouté : ", listArticlesAdded.length,"taille fichier article checké : ",listArticlesChecked.length);
+    }
+};
