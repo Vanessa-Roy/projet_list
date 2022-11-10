3789f40ffb0d7932e9beabc696eb5bcdc30fdd14
vanessa <vr.vanessa.roy@gmail.com>
2022-11-10 13:36:29 +0100

    F3 done (add to the checked list)

--- projet_list.js
+++ projet_list.js
@@ -1,4 +1,5 @@
 const listArticlesAdded = [];
+const listArticlesChecked = [];
 const button = document.getElementById("button");
 button.addEventListener("click",function(event){
     event.preventDefault();
@@ -9,9 +10,24 @@ function addToTheList() {
     listArticlesAdded.push(article);
     let li = document.createElement('li');
     let checkbox = document.createElement('input');
-    li.innerHTML = article;
     checkbox.setAttribute("type","checkbox");
+    checkbox.setAttribute("onclick","addToTheCheckedList(this)");
+    let label = document.createElement('label');
+    label.innerHTML = article;
+    label.appendChild(checkbox);
+    li.appendChild(label);
     articleAdded.appendChild(li);
-    articleAdded.appendChild(checkbox);
-    console.log(listArticlesAdded);
+}
+function addToTheCheckedList(e) {
+    if(e.checked === true) {
+        listArticlesChecked.push(e.parentElement.textContent);
+        let li = document.createElement('li');
+        li.appendChild(e.parentElement);
+        articleChecked.appendChild(li);
+        e.removeAttribute("onclick","addToTheCheckedList(this)");
+        e.setAttribute("type","hidden");
+    } else {
+        console.log("not checked yet");
+    }
+    console.log(listArticlesChecked);
 }
