001a48ecd41712e90d2c94e97729433179aa96f3
vanessa <vr.vanessa.roy@gmail.com>
2022-11-10 15:13:52 +0100

    F4 done (to put back the article checked into the list)

--- projet_list.js
+++ projet_list.js
@@ -17,6 +17,8 @@ function addToTheList() {
     label.appendChild(checkbox);
     li.appendChild(label);
     articleAdded.appendChild(li);
+    console.log("articles ajoutés : ", listArticlesAdded);
+    console.log("articles sélectionnés : ", listArticlesChecked);
 }
 function addToTheCheckedList(e) {
     if(e.checked === true) {
@@ -25,9 +27,32 @@ function addToTheCheckedList(e) {
         li.appendChild(e.parentElement);
         articleChecked.appendChild(li);
         e.removeAttribute("onclick","addToTheCheckedList(this)");
-        e.setAttribute("type","hidden");
+        e.setAttribute("type","button");
+        e.setAttribute("value","remettre dans les articles ajoutés");
+        e.setAttribute("class","backToTheList");
+        e.setAttribute("onclick","removeAndAddToTheList(this)")
+        let indexOfArticleAdded = listArticlesAdded.indexOf(e.parentElement.textContent);
+        let articleAddedRemoved = listArticlesAdded.splice(indexOfArticleAdded, 1);
+        console.log("articles ajoutés : ", listArticlesAdded);
+        console.log("articles sélectionnés : ", listArticlesChecked);
     } else {
         console.log("not checked yet");
     }
-    console.log(listArticlesChecked);
+}
+function removeAndAddToTheList(e) {
+    listArticlesAdded.push(e.parentElement.textContent);
+    let li = document.createElement('li');
+    let checkbox = document.createElement('input');
+    checkbox.setAttribute("type","checkbox");
+    checkbox.setAttribute("onclick","addToTheCheckedList(this)");
+    let label = document.createElement('label');
+    label.innerHTML = e.parentElement.textContent;
+    label.appendChild(checkbox);
+    li.appendChild(label);
+    articleAdded.appendChild(li);
+    let indexOfArticleChecked = listArticlesChecked.indexOf(e.parentElement.textContent);
+    let articleCheckedRemoved = listArticlesChecked.splice(indexOfArticleChecked, 1);
+    e.parentElement.innerHTML = "";
+    console.log("articles ajoutés : ", listArticlesAdded);
+    console.log("articles sélectionnés : ", listArticlesChecked);
 }
